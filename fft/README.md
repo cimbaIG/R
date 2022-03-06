f5aa47890f0dd5e1eb04558e0c29a724a2a64f65
cimbaIG <mihael.cindori@gmail.com>
2022-03-06 15:19:42 +0100

    FFT of a simple sine wave in R.

---
+++ fft/README.md
@@ -0,0 +1,5 @@
+#fft
+
+A script that calculates the FFT of a simple sine wave.
+It is currently tested on the signals with the even number of data.
+Script is tested with R 4.1.2.
---
+++ fft/fft.R
@@ -0,0 +1,73 @@
+#Load libraries
+require(stats)
+library("here")
+
+#Set current directory as working directory
+setwd(here())
+
+#Function returns Discrete Fourier Transform sample frequencies.
+#The sample frequencies are defined according to the Scipy definiton.
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.fftpack.fftfreq.html
+fftfreq <- function(n, d) {
+    #If n is even number (even number of signal points)
+    if (n %% 2 == 0) {
+        init_f <- seq(0, n / 2 - 1, by = 1) / (n * d)
+        f <- append(init_f, - (n / 2) / (d * n), after = length(init_f))
+        neg_f <- seq(- n / 2 + 1, -1, by = 1) / (n * d)
+        f <- append(f, neg_f, length(f))
+    }
+    #If n is odd number (odd number of signal points)
+    else {
+        init_f <- seq(0, (n - 1) / 2, by = 1) / (n * d)
+        f <- append(init_f, - ((n - 1) / 2) / (d * n), after = length(init_f))
+        neg_f <- seq(- (n - 1) / 2 + 1, -1, by = 1) / (n * d)
+        f <- append(f, neg_f, length(f))
+    }
+    return(f)
+}
+
+#Domain setup
+tmax <- 0.75 #maximum time value
+dt <- 0.25 #s
+freq <- 1 #Hz
+amp <- 1 #amplitude
+t <- seq(0, tmax, by = dt) #also try ts function
+n <- length(t) #length of time vector t
+
+#Create signal
+y <- amp * sin(2 * pi * freq * t)
+
+#Create sample frequencies
+f <- fftfreq(n, dt)
+
+#Fourier transform
+fft_y <- fft(y)
+mag <- sqrt(Re(fft_y)^2 + Im(fft_y)^2) * 2 / n
+
+#Print frequency and amplitude values
+cat("Frequencies: ", f[1 : (n / 2)], "\n", sep = "\t")
+cat("Amplitudes: ", mag[1 : (n / 2)], "\n", sep = "\t")
+
+# Writing frequencies and amplitudes data to a tsv file
+frequencies <- as.matrix(f[1 : (n / 2)])
+amplitudes <- as.matrix(mag[1 : (n / 2)])
+results <- cbind(frequencies, amplitudes)
+write.table(results, file = "results.txt", row.names = FALSE,
+    col.names = c("Frequency", "Amplitude"))
+
+#Opening the graphical device
+pdf("fft.pdf")
+
+#Plotting
+layout(matrix(c(1, 2), 2, 1, byrow = TRUE))
+plot(t, y, xlab = "Time t [s]", ylab = "y", type = "l", xlim = c(0, 1),
+    ylim = c(-amp, amp))
+#plot(f[seq(1, length(f), by = 1) / 2],
+#    mag[seq(1, length(f), by = 1) / 2], xlab = "Frequency, Hz",
+#        ylab = "Amplitude", xlim = c(0, 1), ylim = c(0, 1), type = "l")
+plot(f[1 : (n / 2)],
+    mag[1 : (n / 2)], xlab = "Frequency f [Hz]",
+        ylab = "Amplitude A", xlim = c(0, 1), ylim = c(0, 1), type = "l")
+
+#Closing the graphical device
+dev.off()
